f26c7e6f1799018d97e7a9ae335be0c5886a8bae
skiwi <frank_skiwi@hotmail.com>
2016-11-24 14:04:57 +0100

    IntermediateCodeOptimizer now also optimizes MemoryValueChangeExpression nodes, also made sure that all source files are compiled with the UTF-8 encoding.

--- bf-compiler/src/main/java/com/skiwi/bfcompiler/optimizers/IntermediateCodeOptimizer.java
+++ bf-compiler/src/main/java/com/skiwi/bfcompiler/optimizers/IntermediateCodeOptimizer.java
@@ -5,6 +5,7 @@ import com.skiwi.bfcompiler.ast.ASTNode;
 import com.skiwi.bfcompiler.expression.Expression;
 import com.skiwi.bfcompiler.expression.IntegerExpression;
 import com.skiwi.bfcompiler.expression.intermediate.MemoryPointerChangeExpression;
+import com.skiwi.bfcompiler.expression.intermediate.MemoryValueChangeExpression;
 
 import java.util.Collections;
 import java.util.ListIterator;
@@ -25,18 +26,19 @@ public class IntermediateCodeOptimizer {
 
         node.getChildren().forEach(this::optimizeNode);
 
-        // Find consecutive MemoryPointerChangeExpression nodes
+        // Find consecutive MemoryPointerChangeExpression and MemoryValueChangeExpression nodes
         ListIterator<ASTNode> listIterator = node.getChildren().listIterator();
         while (listIterator.hasNext()) {
             ASTNode sentinel = listIterator.next();
             int consecutiveNodes = 1;
-            int changeAmount = (sentinel.getExpression() instanceof MemoryPointerChangeExpression)
+            int changeAmount = (sentinel.getExpression() instanceof MemoryPointerChangeExpression || sentinel.getExpression() instanceof MemoryValueChangeExpression)
                 ? sentinel.getChildExpression(0, IntegerExpression.class).getInteger()
                 : 0;
 
             while (listIterator.hasNext()) {
                 ASTNode next = listIterator.next();
-                if (!(next.getExpression() instanceof MemoryPointerChangeExpression && sentinel.getExpression() instanceof MemoryPointerChangeExpression)) {
+                if (!(next.getExpression() instanceof MemoryPointerChangeExpression && sentinel.getExpression() instanceof MemoryPointerChangeExpression)
+                    && !(next.getExpression() instanceof MemoryValueChangeExpression && sentinel.getExpression() instanceof MemoryValueChangeExpression)) {
                     listIterator.previous();
                     break;
                 }
@@ -49,7 +51,9 @@ public class IntermediateCodeOptimizer {
 
             if (consecutiveNodes >= 2) {
                 listIterator.previous();
-                ASTNode newNode = ASTNode.newWithChild(new MemoryPointerChangeExpression(), new ASTNode(new IntegerExpression(changeAmount)));
+                Expression newExpression = (sentinel.getExpression() instanceof MemoryPointerChangeExpression)
+                    ? new MemoryPointerChangeExpression() : new MemoryValueChangeExpression();
+                ASTNode newNode = ASTNode.newWithChild(newExpression, new ASTNode(new IntegerExpression(changeAmount)));
                 newNode.setParent(node);
                 listIterator.set(newNode);
             }
